f56cf0bd635c27cd7a2e9feb0b986340ff46dabf
vasiliydz <dzyubenko.va@phystech.edu>
2019-05-21 14:02:01 +0300

    Добавлен метод с адаптивным L

--- uigm.py
+++ uigm.py
@@ -5,7 +5,7 @@ from scipy.optimize import approx_fprime
 Y0     = 0
 STEP   = 0.15
 
-def FastDualAveraging(target, x0, eps, set_type = "rn", grad = approx_fprime, step = lambda k: STEP, alpha = lambda k : 1/L):
+def FastDualAveraging(target, x0, eps, set_type = "rn", grad = approx_fprime, step = lambda k: STEP, alpha = lambda k : 1/L, num_of_steps = 10000):
     index = 0
     y_new = Y0
                       
@@ -35,12 +35,12 @@ def FastDualAveraging(target, x0, eps, set_type = "rn", grad = approx_fprime, st
         #print(norm(zk[index] - zk[index-1], ord = 2))
         #if norm(zk[index] - zk[index-1], ord = 2) < eps/2 :
         #    return zk
-        if index >= 10000 :
+        if index >= num_of_steps :
             return zk
         
 
 
-def Gradient(target, x0, eps, L, set_type = "rn", grad = approx_fprime):
+def Gradient(target, x0, eps, L, set_type = "rn", grad = approx_fprime, num_of_steps = 10000):
     xk = [x0]
     index = 0
     while True:
@@ -48,5 +48,55 @@ def Gradient(target, x0, eps, L, set_type = "rn", grad = approx_fprime):
         xk.append(xk[index - 1] - 1/L*grad(xk[index - 1], target, 0.0000001))
         #if norm(xk[index] - xk[index - 1], ord = 2) < eps / 2 :
         #    return xk
-        if index >= 10000 :
+        if index >= num_of_steps :
             return xk
+
+def AdaptiveFDA(target, x0, eps, set_type = "rn", grad = approx_fprime, num_of_steps = 10000):
+    index = 0
+    y_new = Y0
+    
+    grad_sum = np.zeros((len(x0),))
+    L_adapt = 1.
+    alpha = 2./(index+1)
+    zk = [x0]
+    xk = [x0]
+    
+    while True :
+        #print("index = ", index, ", L = ", L_adapt)
+        index += 1
+        y_old = y_new
+        
+        exit_condition = False
+        while not exit_condition:
+            
+            alpha = 2./(index+1)
+            step = (index+1.)/(2*L_adapt)
+            #x_k+1
+            x_new = alpha*y_old + (1 - alpha)*zk[index - 1]
+        
+            grad_sum_tmp = grad_sum + grad(x_new, target, 0.0000001)*step
+            #y_k+1 (шаг метода двойственных усреднений зависит от целевого множества)
+            if set_type == "rn" :
+                y_new = (-1/2*grad_sum_tmp)
+            elif set_type == "simplex" :
+                y_new = (np.exp(-(np.ones(len(x0)) + grad_sum_tmp)))
+            else :
+                print("you oblazhalsya! (rn and simplex are available only)")
+                return zk
+
+            if target(x_new) > target(y_new) + (grad(y_new, target, 0.0000001) @ (x_new - y_new)) + L_adapt/2 * (norm(x_new - y_new, ord = 2) ** 2):
+                L_adapt *= 2
+                exit_condition = False
+            else:
+                xk.append(x_new)
+                grad_sum = grad_sum_tmp
+                #z_k+1
+                zk.append(alpha*(y_old) + (1 - alpha)*zk[index - 1])
+                exit_condition = True
+        #exit condition
+        #print(norm(y_new - y_old, ord = 2))
+        #print(norm(zk[index] - zk[index-1], ord = 2))
+        #if norm(zk[index] - zk[index-1], ord = 2) < eps/2 :
+        #    return zk
+        if index >= num_of_steps :
+            return zk
